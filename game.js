e507e7ebe81eb77b613c684ae0e7d4c154f1e3de
Artem4ikGAletko <158044271+Artem4ikGAletko@users.noreply.github.com>
2024-02-21 18:31:09 +0200

    вирішив переосмислити ворога

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ function create() {
     Bigplatforms.create(400, 568, 'BigP').setScale(2).refreshBody();
 //Гравець
     player = this.physics.add.sprite(100, 450, 'dude');
-
+    
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, platforms);
@@ -87,34 +87,6 @@ function create() {
         repeat: -1
     });
     cursors = this.input.keyboard.createCursorKeys();
-// ворог
-bad = this.physics.add.sprite(150, 450, 'bad');
-
-bad.setBounce(0.2);
-bad.setCollideWorldBounds(true);
-this.physics.add.collider(bad, platforms);
-this.physics.add.collider(bad, Bigplatforms);
-//Анімації
-this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('bad', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-});
-
-this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'bad', frame: 4 } ],
-    frameRate: 20
-});
-
-this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('bad', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-});
-cursors = this.input.keyboard.createCursorKeys();
 
 
 
@@ -122,52 +94,29 @@ cursors = this.input.keyboard.createCursorKeys();
 }
  function update(){
     //керування
+    cursors = this.input.keyboard.createCursorKeys();
     if (cursors.left.isDown)
-{
-    player.setVelocityX(-160);
+    {
+        player.setVelocityX(-160);
+    
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+    
+        player.anims.play('turn');
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 
-    player.anims.play('left', true);
-}
-else if (cursors.right.isDown)
-{
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-}
-else
-{
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-}
-
-if (cursors.up.isDown && player.body.touching.down)
-// djhju
-{
-    player.setVelocityY(-330);
-}
-
-if (cursors.left.isDown)
-{
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-}
-else if (cursors.right.isDown)
-{
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-}
-else
-{
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-}
-
-if (cursors.up.isDown && player.body.touching.down)
-{
-    player.setVelocityY(-330);
-}
-}
+ }
